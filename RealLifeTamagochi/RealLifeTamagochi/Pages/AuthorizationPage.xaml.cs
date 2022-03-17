b55048f73bc8e4efff97148a9e7f3e84adcd8a52
sofahmm <74815523+sofahmm@users.noreply.github.com>
2022-03-17 21:55:01 +0300

    Add project files.

---
+++ RealLifeTamagochi/RealLifeTamagochi/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RealLifeTamagochi.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AuthorizationPage : ContentPage
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_registr_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
